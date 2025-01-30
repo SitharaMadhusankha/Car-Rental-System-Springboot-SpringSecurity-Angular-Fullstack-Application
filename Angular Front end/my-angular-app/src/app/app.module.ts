0112603aa55130f1b27bdfc3504ea885acd1cc3b
SitharaMadhusankha <166124345+SitharaMadhusankha@users.noreply.github.com>
2025-01-30 20:53:06 +0530

    create toggle method

--- Angular Front end/my-angular-app/src/app/app.module.ts
+++ Angular Front end/my-angular-app/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
